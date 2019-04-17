616f8ad28e004886496243ce29e9b53408c2f974
smilesun <smilesun.east@gmail.com>
2019-04-17 18:35:56 +0200

    .

--- docker-togo.sh
+++ docker-togo.sh
@@ -1,10 +1,21 @@
+# nvidio-docker tutorial
+https://docs.nvidia.com/deeplearning/dgx/bp-docker/index.html
+
+# check cuda version support for tensorflow
+https://www.tensorflow.org/install/source#tested_source_configurations
+
 # list all running containers
 docker ps -a
 ## CONTAINER ID IMAGE COMMAND CREATED STATUS...
 
 # re-entering a running docker
 docker exec -it [container-id] bash
+docker attach [container-id]
 
-# pull and run a container
-nvidia-docker run -it tensorflow/tensorflow:1.4.1-gpu
-nvidia-docker run -it tensorflow/tensorflow:1.13.1 
+# pull and run a container i:interactive, t: tty, p: publish port
+nvidia-docker run -it tensorflow/tensorflow:1.4.1-gpu  # 1.4.1 is the latest tensorflow that support cuda 8
+nvidia-docker run -it tensorflow/tensorflow:1.13.1
+
+# debug
+make sure  "nvidia-smi" works
+docker rmi -f <image ID> and rerun command  # does not work
