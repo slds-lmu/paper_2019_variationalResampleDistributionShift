4a2c5cfc96e4c68cc37521045b5fa560855a9ffd
smilesun <smilesun.east@gmail.com>
2019-04-28 23:33:56 +0200

    Fixme:utils appear in subfolder and main folder

--- refactor_Bayesian_CNN/refactor_dataset_class.py
+++ refactor_Bayesian_CNN/refactor_dataset_class.py
@@ -1,7 +1,40 @@
 import tensorflow as tf
+import numpy as np
+import os, sys
+os.getcwd()
+import os.path
+print(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
+import utils
+# FIXME: the problem is the folder above has the same module named utils
+from utils import *
+from data_generator import concatenate_data_from_dir
+#os.path.realpath(__file__)
+#sys.path.append(os.path.dirname(os.path.dirname()))
+
+data_path = '../results/VAE_fashion-mnist_64_62'
+result_path = '../results/VAE_fashion-mnist_64_62'
+if not tf.gfile.Exists(data_path+"/global_index_cluster_data.npy"):
+    _,global_index = concatenate_data_from_dir(data_path,num_labels=num_labels,num_clusters=num_cluster)
+else:global_index = np.load(data_path+"/global_index_cluster_data.npy",allow_pickle=True)
+# type(global_index)  numpy.ndarray
+len(global_index.item().get(str(1)))
+
+a = global_index.item().get(str(0))
+b = global_index.item().get(str(1))
+np.append(a, b)
+np.concatenate((a, b))
+gen = (a for a in [[2,4], [1,5,7]])
+print(list(gen))  # can only be executed once
+import sys
+map(sys.stdout.write, gen)
+from itertools import chain
+gen2 = chain(gen)
+list(gen2).ravel()
+
 class VGMMDataset(Dataset):
     """Dataset after VGMM clustering"""
-    def __init__(self, pattern, root_dir, transform=None, list_idx):
+    def __init__(self, pattern = "/global_index_cluster_data.npy", root_dir = '../results/VAE_fashion-mnist_64_62', transform=None, list_idx):
         """
         Args:
             pattern (string): Path to the npy file.
@@ -10,26 +43,25 @@ class VGMMDataset(Dataset):
                 on a sample.
             list_idx (list): the list of indexes of the cluster to choose as trainset or testset
         """
-        if not tf.gfile.Exists(data_path+"/global_index_cluster_data.npy"):
-        _,global_index = concatenate_data_from_dir(data_path,num_labels=num_labels,num_clusters=num_cluster)
-        else:global_index = np.load(data_path+"/global_index_cluster_data.npy",allow_pickle=True)
-        self.landmarks_frame = pd.read_csv(csv_file)
+        X,y = load_mnist(args.dataset)
         self.root_dir = root_dir
+        self.pattern = pattern
         self.transform = transform
+        if not tf.gfile.Exists(data_path+ pattern):
+        _, self.global_index = concatenate_data_from_dir(data_path,num_labels=num_labels,num_clusters=num_cluster)
+        else:self.global_index = np.load(data_path + pattern,allow_pickle=True)
+        self.list_idx = list_idx
+        all_inds = []
+        for index in self$list_idx:
+            to_append = self.global_index.item().get(str(index))
+            all_inds = np.append(all_inds, to_append)
+        self.samples = self$X[all_inds]
 
     def __len__(self):
         return len(self.landmarks_frame)
 
     def __getitem__(self, idx):
-        global_index.item().get(str(self$cluster_index))
-
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:].as_matrix()
-        landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
-
+        sample = self$samples[idx, ]
         if self.transform:
             sample = self.transform(sample)
 
