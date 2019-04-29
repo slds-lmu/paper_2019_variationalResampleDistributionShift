d7d55f69e9e2372800e02899544926c527274781
smilesun <smilesun.east@gmail.com>
2019-04-30 01:54:59 +0200

    copy non-final result first

--- copy_result.sh
+++ copy_result.sh
@@ -1,6 +1,5 @@
+cp ~/lrzDatalab/paper_2019_rfms_representation/refactor_Bayesian_CNN/diagnostics_Bayes3conv3fc* .    # copy non final result first
 cp ~/lrzDatalab/paper_2019_rfms_representation/refactor_Bayesian_CNN/rand_cross_validation_result.p .
 cp ~/lrzDatalab/paper_2019_rfms_representation/refactor_Bayesian_CNN/rand_cross_validation_result.json .
 cp ~/lrzDatalab/paper_2019_rfms_representation/refactor_Bayesian_CNN/vgmm_cross_validation_result.p .
 cp ~/lrzDatalab/paper_2019_rfms_representation/refactor_Bayesian_CNN/vgmm_cross_validation_result.json .
-cp ~/lrzDatalab/paper_2019_rfms_representation/refactor_Bayesian_CNN/diagnostics_Bayes3conv3fc* .
-
