a14524165b011453246d5c0f4f2d363339b4045e
agisga <11449372+agisga@users.noreply.github.com>
2019-05-02 22:45:05 -0400

    update plots: Bayesian and NonBayesian AlexNet completely done

--- plots/learning_curve.R
+++ plots/learning_curve.R
@@ -1,32 +1,42 @@
 library(tidyverse)
 
-parse_data <- function(base_filename, epochs = 30, folds = 5) {
+parse_data <- function(base_filename, epochs = 30) {
 
   full_df <- data_frame()
-  for (i in 0:folds) {
-    for (train_or_test in c("train", "test")) {
+  for (i in 0:5) {
+    for (dataset in c("train", "val", "test")) {
       for (rand in c("CV", "VGMM-CV")) {
         if (rand == "CV") {
           filename <- paste0("./data/", base_filename,
-                             i, "_", train_or_test, "_rand.txt")
+                             i, "_", dataset, "_rand.txt")
         } else {
           filename <- paste0("./data/", base_filename,
-                             i, "_", train_or_test, ".txt")
+                             i, "_", dataset, "_vgmm.txt")
         }
 
         if (file.exists(filename)) {
           print(paste0("Reading CV fold #",
-                       i, ", ", train_or_test, " set, ",
+                       i, ", ", dataset, " set, ",
                        "and rand = ", rand))
           cv_txt <- read_delim(filename, delim = "}", col_names = FALSE,
                                col_types = cols(.default = "c"))
         } else {
+          warning(paste(filename, "does not exist!"))
           next
         }
 
+        # There is a bug where the VGMM folds are numbered from 0 to 4 but
+        # the random CV folds are numbered from 1 to 5. Taking care of that:
+        if (rand == "CV") {
+          cv_txt <- mutate(cv_txt, cv = i)
+        } else {
+          cv_txt <- mutate(cv_txt, cv = i + 1)
+        }
+
+        key <- list(train = "Train", val = "Validation", test = "Test")
         cv_df <- cv_txt %>%
-          mutate(cv =  i, train_or_test = train_or_test, rand = rand) %>%
-          gather(observation, value, -cv, -train_or_test, -rand) %>%
+          mutate(dataset = key[[dataset]], rand = rand) %>%
+          gather(observation, value, -cv, -dataset, -rand) %>%
           drop_na()
 
         cv_df <- cv_df %>%
@@ -41,46 +51,22 @@ parse_data <- function(base_filename, epochs = 30, folds = 5) {
 
         if (!all(complete.cases(cv_df))) {
           stop(paste0("Missing values for CV #",
-                      i, ", ", train_or_test, " set, ",
+                      i, ", ", dataset, " set, ",
                       "and rand = ", rand))
         }
 
         # dirty hacks:
-        # need only take the last set of epoch, which are indexed as 1,2,..,k in a
-        # contiguous block of the dataframe rows, with k<30 for runs that didn't finish or k=30 for those that did.
-        # TODO: Can (mostly?) be removed once all runs are completely finished, and the results are cleaned up.
-        ind30 <- which(cv_df$epoch == epochs)
+        # Epoch 1 appears twice for the VGMM results for some reason.
         if (rand == "VGMM-CV") {
-          if (length(ind30) == 0) {
-            # do nothing
-          } else if (length(ind30) == 1) {
-            if (ind30 + 1 < nrow(cv_df)) {
-              cv_df <- cv_df[(ind30 + 1):nrow(cv_df), ]
-            }
-          } else if (length(ind30) == 2) {
-            if (nrow(cv_df) == ind30[2]) {
-              cv_df <- cv_df[(ind30[1] + 1):nrow(cv_df), ]
-            } else {
-              cv_df <- cv_df[(ind30[2] + 1):nrow(cv_df), ]
-            }
-          } else if (length(ind30) == 3) {
-            cv_df <- cv_df[(ind30[1] + 1):nrow(cv_df), ]
-          } else if (length(ind30) == 4) {
-            cv_df <- cv_df[(ind30[2] + 1):(nrow(cv_df)-1), ]
-          } else {
-            stop(paste0("Unexpected number of repeted epochs for CV #",
-                        i, ", ", train_or_test, " set, ",
-                        "and rand = ", rand))
-          }
-        } else if (rand == "CV") {
-          if (nrow(cv_df) >= epochs) {
-            cv_df <- cv_df[1:epochs, ]
+          ind1 <- which(cv_df$epoch == 1)
+          if (length(ind1) > 1) {
+            cv_df <- cv_df[ind1[length(ind1)]:nrow(cv_df), ]
           }
         }
 
-        if ( !( all(sort(cv_df$epoch) == 1:epochs) | length(cv_df) < epochs) ) {
+        if ( !all(sort(cv_df$epoch) == 1:epochs )) {
           stop(paste0("Unexpected number of repeted epochs for CV #",
-                      i, ", ", train_or_test, " set, ",
+                      i, ", ", dataset, " set, ",
                       "and rand = ", rand))
         }
 
@@ -95,35 +81,7 @@ parse_data <- function(base_filename, epochs = 30, folds = 5) {
 
 #--- Bayesian 3conv3fc
 
-# parse the data
-
-Bayes3conv3fc_mnist_cv_df <- parse_data("diagnostics_Bayes3conv3fc_mnist_cv")
-table(complete.cases(Bayes3conv3fc_mnist_cv_df))
-
-# plot accuracy
-
-Bayes3conv3fc_mnist_cv_df %>%
-  mutate(fold = factor(cv)) %>%
-  ggplot(aes(epoch, accuracy, color = fold, shape = fold, linetype = fold)) +
-    geom_point() + geom_line() +
-    facet_grid(rand~train_or_test) +
-    theme_bw() + xlab("Epoch") + ylab("Accuracy")
-
-ggsave("./output/Bayes3conv3fc_mnist_cv_accuracy.png")
-ggsave("./output/Bayes3conv3fc_mnist_cv_accuracy.pdf")
-
-# plot loss
-
-Bayes3conv3fc_mnist_cv_df %>%
-  mutate(fold = factor(cv)) %>%
-  ggplot(aes(epoch, loss, color = fold, shape = fold, linetype = fold)) +
-    geom_point() + geom_line() +
-    facet_grid(rand~train_or_test) +
-    theme_bw() + xlab("Epoch") + ylab("Loss")
-
-ggsave("./output/Bayes3conv3fc_mnist_cv_loss.png")
-ggsave("./output/Bayes3conv3fc_mnist_cv_loss.pdf")
-
+# TODO: this needs new data
 
 #--- Non-Bayesian AlexNet
 
@@ -131,16 +89,20 @@ ggsave("./output/Bayes3conv3fc_mnist_cv_loss.pdf")
 
 NonBayesalexnet_mnist_cv_df <- parse_data("diagnostics_NonBayesalexnet_mnist_cv",
                                           epochs = 100)
-table(complete.cases(Bayes3conv3fc_mnist_cv_df))
+table(complete.cases(NonBayesalexnet_mnist_cv_df))
 
 # plot accuracy
 
+NonBayesalexnet_mnist_cv_df$dataset <- factor(NonBayesalexnet_mnist_cv_df$dataset,
+                                              ordered = TRUE,
+                                              levels = c("Train", "Validation", "Test"))
 NonBayesalexnet_mnist_cv_df %>%
-  mutate(fold = factor(cv)) %>%
-  ggplot(aes(epoch, accuracy, color = fold, shape = fold)) +
+  mutate(Fold = factor(cv)) %>%
+  ggplot(aes(epoch, accuracy, color = Fold, linetype = Fold)) +
     geom_line() +
-    facet_grid(rand~train_or_test) +
-    theme_bw() + xlab("Epoch") + ylab("Accuracy")
+    facet_grid(rand~dataset) +
+    theme_bw() + xlab("Epoch") + ylab("Accuracy") +
+    ggtitle("AlexNet (non-Bayesian) on Fashion-MNIST")
 
 ggsave("./output/NonBayesalexnet_mnist_cv_accuracy.png")
 ggsave("./output/NonBayesalexnet_mnist_cv_accuracy.pdf")
@@ -148,11 +110,12 @@ ggsave("./output/NonBayesalexnet_mnist_cv_accuracy.pdf")
 # plot loss
 
 NonBayesalexnet_mnist_cv_df %>%
-  mutate(fold = factor(cv)) %>%
-  ggplot(aes(epoch, loss, color = fold, shape = fold)) +
+  mutate(Fold = factor(cv)) %>%
+  ggplot(aes(epoch, loss, color = Fold, linetype = Fold)) +
     geom_line() +
-    facet_grid(rand~train_or_test) +
-    theme_bw() + xlab("Epoch") + ylab("Loss")
+    facet_grid(rand~dataset) +
+    theme_bw() + xlab("Epoch") + ylab("Loss") +
+    ggtitle("AlexNet (non-Bayesian) on Fashion-MNIST")
 
 ggsave("./output/NonBayesalexnet_mnist_cv_loss.png")
 ggsave("./output/NonBayesalexnet_mnist_cv_loss.pdf")
@@ -164,16 +127,20 @@ ggsave("./output/NonBayesalexnet_mnist_cv_loss.pdf")
 
 Bayesalexnet_mnist_cv_df <- parse_data("diagnostics_Bayesalexnet_mnist_cv",
                                        epochs = 100)
-table(complete.cases(Bayes3conv3fc_mnist_cv_df))
+table(complete.cases(Bayesalexnet_mnist_cv_df))
 
 # plot accuracy
 
+Bayesalexnet_mnist_cv_df$dataset <- factor(Bayesalexnet_mnist_cv_df$dataset,
+                                           ordered = TRUE,
+                                           levels = c("Train", "Validation", "Test"))
 Bayesalexnet_mnist_cv_df %>%
-  mutate(fold = factor(cv)) %>%
-  ggplot(aes(epoch, accuracy, color = fold, shape = fold)) +
+  mutate(Fold = factor(cv)) %>%
+  ggplot(aes(epoch, accuracy, color = Fold, linetype = Fold)) +
     geom_line() +
-    facet_grid(rand~train_or_test) +
-    theme_bw() + xlab("Epoch") + ylab("Accuracy")
+    facet_grid(rand~dataset) +
+    theme_bw() + xlab("Epoch") + ylab("Accuracy") +
+    ggtitle("Bayesian AlexNet on Fashion-MNIST")
 
 ggsave("./output/Bayesalexnet_mnist_cv_accuracy.png")
 ggsave("./output/Bayesalexnet_mnist_cv_accuracy.pdf")
@@ -181,11 +148,12 @@ ggsave("./output/Bayesalexnet_mnist_cv_accuracy.pdf")
 # plot loss
 
 Bayesalexnet_mnist_cv_df %>%
-  mutate(fold = factor(cv)) %>%
-  ggplot(aes(epoch, loss, color = fold, shape = fold)) +
+  mutate(Fold = factor(cv)) %>%
+  ggplot(aes(epoch, loss, color = Fold, shape = Fold)) +
     geom_line() +
-    facet_grid(rand~train_or_test) +
-    theme_bw() + xlab("Epoch") + ylab("Loss")
+    facet_grid(rand~dataset) +
+    theme_bw() + xlab("Epoch") + ylab("Loss") +
+    ggtitle("Bayesian AlexNet on Fashion-MNIST")
 
 ggsave("./output/Bayesalexnet_mnist_cv_loss.png")
 ggsave("./output/Bayesalexnet_mnist_cv_loss.pdf")
